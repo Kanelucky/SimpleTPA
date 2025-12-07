07c21cc59704f7782f028ba12643cc0ca79701dd
daoge_cmd <3523206925@qq.com>
2025-12-07 18:34:08 +0800

    refactor: replace lombok logger with pluginLogger

--- src/main/java/org/allaymc/javaplugintemplate/JavaPluginTemplate.java
+++ src/main/java/org/allaymc/javaplugintemplate/JavaPluginTemplate.java
@@ -1,22 +1,20 @@
 package org.allaymc.javaplugintemplate;
 
-import lombok.extern.slf4j.Slf4j;
 import org.allaymc.api.plugin.Plugin;
 
-@Slf4j
 public class JavaPluginTemplate extends Plugin {
     @Override
     public void onLoad() {
-        log.info("JavaPluginTemplate loaded!");
+        this.pluginLogger.info("JavaPluginTemplate is loaded!");
     }
 
     @Override
     public void onEnable() {
-        log.info("JavaPluginTemplate enabled!");
+        this.pluginLogger.info("JavaPluginTemplate is enabled!");
     }
 
     @Override
     public void onDisable() {
-        log.info("JavaPluginTemplate disabled!");
+        this.pluginLogger.info("JavaPluginTemplate is disabled!");
     }
 }
